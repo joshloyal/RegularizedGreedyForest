b97ae8dd8a9232ae3dfe5c3d8d1e1ff9f178eb09
Joshua Loyal <joshua.d.loyal@gmail.com>
2016-05-30 14:47:07 -0400

    mvoed around test directory

--- rgforest/tests/test_rgf.py
+++ rgforest/tests/test_rgf.py
@@ -1,81 +1,58 @@
 import numpy as np
+import os
 
 import sklearn.metrics as metrics
 
 from rgforest import rgf
 from rgforest import dataset
+from rgforest.tests.test_utils import get_test_data
 
 module_rng = np.random.RandomState(1234)
 
-def get_test_data(n_train=1000, n_test=500, n_features=10, n_categoricals=0,
-                  n_classes=2, classification=True, random_seed=1234):
-    """Classification or regression problem that should be easily solvable by a gbm
-
-    Parameters
-    ----------
-    n_train : int
-        number of training examples
-    n_test : int
-        number of test examples
-    n_features : int
-        number of features in the synthetic dataset
-    n_categorical : int
-        number of categorical features to be present
-    n_classes : int
-        number of classes for a classification target
-    classification : bool (default=True)
-        Classification problem if True, otherwise regression
-    random_seed : int
-        Seed for the random number generator
-    """
-    rng = np.random.RandomState(random_seed)
-    n_samples = n_train + n_test
-
-    n_features = n_features - n_categoricals
-    if n_features < 0:
-        raise ValueError('Requested more categoricals than n_features')
-
-    if classification:
-        #  guassian blobs
-        y = rng.randint(0, n_classes, size=(n_samples,)).astype(np.float64)
-        X = np.zeros((n_samples, n_features))
-        for i in range(n_samples):
-            X[i] = rng.normal(loc=y[i], scale=0.7, size=(n_features,))
-    else:
-        y_loc = rng.random_sample((n_samples,))
-        X = np.zeros((n_samples, n_features))
-        y = np.zeros((n_samples, 1)).astype(np.float64)
-        for i in range(n_samples):
-            X[i] = rng.normal(loc=y_loc[i], scale=0.7, size=(n_features,))
-            y[i] = rng.normal(loc=y_loc[i], scale=0.7, size=(1,))
-        y = y.ravel()
-
-    if n_categoricals:
-        for _ in range(n_categoricals):
-            X = np.hstack((X, rng.choice(range(10), n_samples)[:, np.newaxis]))
-
-    return (X[:n_train], y[:n_train]), (X[n_train:], y[n_train:])
 
 
 def test_train_classification():
     (X_train, y_train), (X_test, y_test) = get_test_data()
-    est = rgf.RegularizedGreedyForestEstimator(l2=0.01, max_leaf_nodes=500, verbose=1)
+    est = rgf.RegularizedGreedyForestClassifier(l2=0.01, max_leaf_nodes=500)
     est.fit(X_train, y_train)
 
     y_pred = est.predict(X_train)
-    y_pred = (y_pred > 0.5).astype(np.int)
     train_score = metrics.accuracy_score(y_train, y_pred)
-    assert train_score > 0.5
+    assert train_score > 0.75
 
     y_pred = est.predict(X_test)
-    y_pred = (y_pred > 0.5).astype(np.int)
     test_score = metrics.accuracy_score(y_test, y_pred)
-    assert test_score > 0.5
+    assert test_score > 0.75
+
+    y_proba = est.predict_proba(X_test)
+    test_score = metrics.roc_auc_score(y_test, y_proba)
+    assert test_score > 0.75
+
+
+def test_train_classification_weighted():
+    (X_train, y_train), (X_test, y_test) = get_test_data()
+    sample_weight = np.ones_like(y_train)
+    sample_weight[y_train == 0] = 0.5
+
+    est = rgf.RegularizedGreedyForestClassifier(l2=0.01, max_leaf_nodes=500)
+    est.fit(X_train, y_train, sample_weight)
+
+    y_pred = est.predict(X_train)
+    train_score = metrics.accuracy_score(y_train, y_pred)
+    assert train_score > 0.75
+
+    y_pred = est.predict(X_test)
+    test_score = metrics.accuracy_score(y_test, y_pred)
+    assert test_score > 0.75
+
+    y_proba = est.predict_proba(X_test)
+    test_score = metrics.roc_auc_score(y_test, y_proba)
+    assert test_score > 0.75
 
 
 def test_train_regression():
     (X_train, y_train), (X_test, y_test) = get_test_data(classification=False)
-    est = rgf.RegularizedGreedyForestEstimator(l2=0.01, max_leaf_nodes=500, loss='LS')
+    est = rgf.RegularizedGreedyForestRegressor(l2=0.01, max_leaf_nodes=500)
     est.fit(X_train, y_train)
 
     y_pred = est.predict(X_train)
@@ -85,6 +62,35 @@ def test_train_regression():
     y_pred = est.predict(X_test)
     test_score = metrics.mean_squared_error(y_test, y_pred)
     assert test_score < 2
+
+def test_train_regression_weighted():
+    (X_train, y_train), (X_test, y_test) = get_test_data(classification=False)
+    sample_weight = np.ones_like(y_train)
+    sample_weight[y_train < 0] = 0.5
+
+    est = rgf.RegularizedGreedyForestRegressor(l2=0.01, max_leaf_nodes=500)
+    est.fit(X_train, y_train, sample_weight)
+
+    y_pred = est.predict(X_train)
+    train_score = metrics.mean_squared_error(y_train, y_pred)
+    assert train_score < 2
+
+    y_pred = est.predict(X_test)
+    test_score = metrics.mean_squared_error(y_test, y_pred)
+    assert test_score < 2
+
+
+def test_save_model():
+    (X_train, y_train), (X_test, y_test) = get_test_data()
+    est = rgf.RegularizedGreedyForestClassifier(l2=0.01, max_leaf_nodes=500)
+    est.fit(X_train, y_train)
+
+    file_name = 'test_model'
+    try:
+        est.save(file_name)
+    finally:
+        os.remove(file_name)
+
 
 def test_dataset_ones_wide():
     n_features = 100
---
+++ rgforest/tests/test_utils.py
@@ -0,0 +1,50 @@
+import numpy as np
+
+def get_test_data(n_train=1000, n_test=500, n_features=10, n_categoricals=0,
+                  n_classes=2, classification=True, random_seed=1234):
+    """Classification or regression problem that should be easily solvable by a gbm
+
+    Parameters
+    ----------
+    n_train : int
+        number of training examples
+    n_test : int
+        number of test examples
+    n_features : int
+        number of features in the synthetic dataset
+    n_categorical : int
+        number of categorical features to be present
+    n_classes : int
+        number of classes for a classification target
+    classification : bool (default=True)
+        Classification problem if True, otherwise regression
+    random_seed : int
+        Seed for the random number generator
+    """
+    rng = np.random.RandomState(random_seed)
+    n_samples = n_train + n_test
+
+    n_features = n_features - n_categoricals
+    if n_features < 0:
+        raise ValueError('Requested more categoricals than n_features')
+
+    if classification:
+        #  guassian blobs
+        y = rng.randint(0, n_classes, size=(n_samples,)).astype(np.float64)
+        X = np.zeros((n_samples, n_features))
+        for i in range(n_samples):
+            X[i] = rng.normal(loc=y[i], scale=0.7, size=(n_features,))
+    else:
+        y_loc = rng.random_sample((n_samples,))
+        X = np.zeros((n_samples, n_features))
+        y = np.zeros((n_samples, 1)).astype(np.float64)
+        for i in range(n_samples):
+            X[i] = rng.normal(loc=y_loc[i], scale=0.7, size=(n_features,))
+            y[i] = rng.normal(loc=y_loc[i], scale=0.7, size=(1,))
+        y = y.ravel()
+
+    if n_categoricals:
+        for _ in range(n_categoricals):
+            X = np.hstack((X, rng.choice(range(10), n_samples)[:, np.newaxis]))
+
+    return (X[:n_train], y[:n_train]), (X[n_train:], y[n_train:])
