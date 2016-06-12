4dad92c3cf5f1c38fd58ac28418d162158ae53b1
Joshua Loyal <joshua.d.loyal@gmail.com>
2016-06-12 15:45:26 -0400

    fixed term typo

--- build_tools/test_script.sh
+++ build_tools/test_script.sh
@@ -3,7 +3,7 @@ set -e
 
 # run tests
 if [[ "$COVERAGE" == "true" ]]; then
-    py.test --cov=rgforest --cov-report=trem --cov-report=html -s rgforest
+    py.test --cov=rgforest --cov-report=term --cov-report=html -s rgforest
 else
     py.test -s rgforest
 fi
