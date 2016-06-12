d6f4dd3f52538a9be7bf78e2461da42d8f3bfe38
Joshua Loyal <joshua.d.loyal@gmail.com>
2016-06-12 16:11:30 -0400

    added flake8 test

--- build_tools/test_script.sh
+++ build_tools/test_script.sh
@@ -4,6 +4,8 @@ set -e
 # run tests
 if [[ "$COVERAGE" == "true" ]]; then
     py.test --cov=rgforest --cov-report=term --cov-report=html -s rgforest
+elif [[ "$TESTMODE" == "FLAKE8" ]]; then
+    py.test --flake8 rgforest
 else
     py.test -s rgforest
 fi
